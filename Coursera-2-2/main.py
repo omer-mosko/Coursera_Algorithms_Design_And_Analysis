5d6edad8c46e23e7978b8d6bdab1d478230c4ec7
omer-mosko <omer.mosko@gmail.com>
2022-10-18 16:28:36 +0300

    commit

--- Coursera-2-2/main.py
+++ Coursera-2-2/main.py
@@ -21,12 +21,33 @@ class Graph():
     def print_graph(self):
         for src, dst in self.adjlist.items():
             print("From ", src, " to ", ', '.join(' weight '.join((str(elm) for elm in tup)) for tup in dst))
-    def dijkstra(self, src):
-        visited = [False for _ in range(len|(1))]
-        dist = [np.Inf for _ in range(n)]
-        #while sum(visited) < self.V:
-            #for v in
-G = Graph('test1.txt')
-G.add_edge(1, 6, 20)
-print("dd", G.get_neighbours(1))
-G.print_graph()
+        # return
+    def findMin(self, dist, visited):
+        min = np.Inf
+        for v in self.adjlist:
+            if v not in visited and dist[v - 1] < min:
+                min = dist[v - 1]
+                minV = v
+        return minV
+    def dijkstra(self, src, printV):
+        visited = set()
+        dist = [np.Inf for _ in range(self.V)]
+        dist[src] = 0
+        while len(visited) < self.V:
+            # for v in self.adjlist:
+            u = self.findMin(dist, visited)
+            visited.add(u)
+            for vertex, weight in self.adjlist[u]:
+                if vertex not in visited and dist[vertex - 1] > dist[u - 1] + weight:
+                    dist[vertex - 1] = dist[u - 1] + weight
+        for i, v in enumerate(printV):
+            print(dist[v - 1], end = "," if i != len(printV) - 1 else "")
+G = Graph('DIjkstraData.txt')
+# G = Graph('test1.txt')
+# 7,37,59,82,99,115,133,165,188,197
+# G.add_edge(1, 6, 20)
+# print("dd", G.get_neighbours(1))
+# G.print_graph()
+G.dijkstra(0, [7,37,59,82,99,115,133,165,188,197])
+# G.dijkstra(0, [6,7,8])
+
